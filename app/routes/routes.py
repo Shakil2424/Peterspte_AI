12251a96d97f959b494c1a33daac5797c4ea5fb7
iamyeasirarafat <yas.yeasir@gmail.com>
2025-06-29 14:46:13 +0600

    Refactor app structure: consolidate route registration and enhance .gitignore

---
+++ app/routes/routes.py
@@ -0,0 +1,9 @@
+from .asq_routes import asq_bp
+from .transcription_routes import transcription_bp
+
+
+routes = [
+    asq_bp,
+    transcription_bp,
+    # Add more routers here
+]
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from flask import Flask
-from app.routes.transcription_routes import transcription_bp
-from app.routes.asq_routes import asq_bp
+from app.routes import routes
+
 
 def create_app():
     app = Flask(__name__)
-    
-    # Configure upload folder
-    app.config['UPLOAD_FOLDER'] = 'uploads'
 
-    # Register blueprints
-    app.register_blueprint(transcription_bp)
-    app.register_blueprint(asq_bp)
+    # Configure upload folder
+    app.config["UPLOAD_FOLDER"] = "uploads"
+
+    # Register all blueprints
+    for bp in routes:
+        app.register_blueprint(bp)
 
     return app
 
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True, port=5000)
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=False, port=6000)
